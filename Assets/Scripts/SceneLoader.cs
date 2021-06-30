12984f20447941ca7c0369f13177e4b41543d96d
Daniele Tabanella <daniele.tabanella@gmail.com>
2021-06-30 23:59:50 +0200

    Initial commit

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadNextScene() {
+        int currentsceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentsceneIndex + 1);
+    }
+    public void LoadStartScene() {
+        SceneManager.LoadScene(0);
+    }
+
+    public void QuitGame() {
+        Application.Quit();
+    }
+}
